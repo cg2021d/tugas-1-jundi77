b9fde42ab3ed1121ba9b9c4549a865f70ccb3367
Jundullah H. R <jundihanif77@gmail.com>
2021-10-06 03:16:41 +0700

    crop svg image

--- tool/convert_coord_to_webgl_friendly.js
+++ tool/convert_coord_to_webgl_friendly.js
@@ -1,6 +1,6 @@
 //* PASTE THIS CODE TO WEB CONSOLE AND CALL convert(...) WITH JSON'S URL RESULT FROM svg_to_json.js
 
-function to_webgl_coord(json, min_x, min_y, canvas_size) {
+function to_webgl_coord(json, min_x_webgl, min_y_webgl, canvas_size) {
     /**
      * JSON STRUCTURE
      * 
@@ -13,28 +13,56 @@ function to_webgl_coord(json, min_x, min_y, canvas_size) {
      * }
      */
     let data = json.data
-    let max = undefined
+    let min_x = undefined
+    let min_y = undefined
+    let max_x = undefined
+    let max_y = undefined
 
     // FIND THE BIGGEST COORDINATE
     for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i][0].length; j++) {
-            for (let k = 0; k < data[i][0][j].length; k++) {
-                if (max == undefined) {
-                    max = data[i][0][j][k];
-                } else if (max < data[i][0][j][k]) {
-                    max = data[i][0][j][k];
-                }
+            // for (let k = 0; k < data[i][0][j].length; k++) {
+            //     if (max == undefined) {
+            //         max = data[i][0][j][k];
+            //     } else if (max < data[i][0][j][k]) {
+            //         max = data[i][0][j][k];
+            //     }
+            // }
+            if (min_x == undefined) {
+                min_x = data[i][0][j][0];
+            } else if (min_x > data[i][0][j][0]) {
+                min_x = data[i][0][j][0];
             }
+            
+            if (max_x == undefined) {
+                max_x = data[i][0][j][0];
+            } else if (max_x < data[i][0][j][0]) {
+                max_x = data[i][0][j][0];
+            }
+            
+            if (min_y == undefined) {
+                min_y = data[i][0][j][1];
+            } else if (min_y > data[i][0][j][1]) {
+                min_y = data[i][0][j][1];
+            }
+
+            if (max_y == undefined) {
+                max_y = data[i][0][j][1];
+            } else if (max_y < data[i][0][j][1]) {
+                max_y = data[i][0][j][1];
+            }
+
         }
     }
 
-    console.log('MAX is: ' + max)
+    let y_size = Math.abs(max_y - min_y);
+    let x_size = Math.abs(max_x - min_x);
 
     // CONVERT COORDINATE
     for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i][0].length; j++) {
-            data[i][0][j][0] = min_x + data[i][0][j][0] / max * canvas_size;
-            data[i][0][j][1] = min_y + data[i][0][j][1] / max * canvas_size;
+            data[i][0][j][0] = min_x_webgl + (data[i][0][j][0] - min_x) / x_size * canvas_size;
+            data[i][0][j][1] = min_y_webgl + (data[i][0][j][1] - min_y) / y_size * canvas_size;
         }
     }
 
